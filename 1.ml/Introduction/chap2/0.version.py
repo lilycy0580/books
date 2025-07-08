b07e050cf20627ccdb65af98a9f51c5e33644877
cy <835842667@qq.com>
2025-07-08 21:15:41 +0800

    2.2 k近邻分类

---
+++ 1.ml/Introduction/chap2/0.version.py
@@ -0,0 +1,20 @@
+import sys
+print("Python version:", sys.version)   # 书本:3.7.6   cy:3.8.20
+
+import pandas as pd
+print("pandas version:", pd.__version__)
+
+import matplotlib
+print("matplotlib version:", matplotlib.__version__)
+
+import numpy as np
+print("NumPy version:", np.__version__)
+
+import scipy as sp
+print("SciPy version:", sp.__version__)
+
+import IPython
+print("IPython version:", IPython.__version__)
+
+import sklearn
+print("scikit-learn version:", sklearn.__version__)
